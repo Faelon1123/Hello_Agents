2487d20e5ca8830460399c2d152c2924bf7e361c
lzf <1516795316@qq.com>
2026-08-23 22:57:52 +0800

    AIHubMix平台API(OpenAI 兼容接口)

--- chapter6/CAMEL/DigitalBookWriting.py
+++ chapter6/CAMEL/DigitalBookWriting.py
@@ -11,9 +11,9 @@ LLM_API_KEY = os.getenv("LLM_API_KEY")
 LLM_BASE_URL = os.getenv("LLM_BASE_URL")
 LLM_MODEL = os.getenv("LLM_MODEL")
 
-#创建模型,在这里以Qwen为例,调用的百炼大模型平台API
+#创建模型,在这里以Qwen为例,调用的AIHubMix平台API(OpenAI 兼容接口)
 model = ModelFactory.create(
-    model_platform=ModelPlatformType.QWEN,
+    model_platform=ModelPlatformType.OPENAI_COMPATIBLE_MODEL,
     model_type=LLM_MODEL,
     url=LLM_BASE_URL,
     api_key=LLM_API_KEY
